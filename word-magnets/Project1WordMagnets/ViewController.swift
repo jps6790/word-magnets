7b8d39f643050b684312acaa337776998b149aab
Student <student@howard03.istlabs.rit.edu>
2018-03-05 00:22:29 -0500

    Share and Image save added

--- word-magnets/Project1WordMagnets/ViewController.swift
+++ word-magnets/Project1WordMagnets/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
     // Word banks
     let words = ["could","cloud","bot","bit","ask","a","geek","flame","file","ed","ed","create","like","lap","is","ing","I","her","drive","get","soft","screen","protect","online","meme","to","they","that","tech","space","source","y","write","while"]
@@ -17,10 +17,46 @@ class ViewController: UIViewController {
     
     let words3 = ["beauty","bare","at","as","as","an","a","a","ed","dream","cry","but","forest","fiddle","fast","er","enormous","lather","it","it","his","moon","sausage","lagomorph","rain","song","to","wind","were","together","you","boy","bear"]
     
+    var backgroundImage: UIImage?
+    
+    @IBAction func share(_ sender: AnyObject) {
+        let image = self.view.takeSnapshot()
+        let textToShare = "I made a poem!"
+        let igmWebsite = NSURL(string: "http://igm.rit.edu/")
+        let objectsToShare:[AnyObject] = [textToShare as AnyObject, igmWebsite!,image!]
+        let activityVC = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
+        activityVC.excludedActivityTypes = [UIActivityType.print]
+        self.present(activityVC, animated: true, completion: nil)
+    }
+    @IBAction func setImageBG(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    ///VIEW DID LOAD FUNCTION
     override func viewDidLoad() {
         super.viewDidLoad()
         
         placeWords()
+    }
+    
+    
+    //MARK: - UIPickerController Delegate Methods
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]){
+        print("finished picking")
+        let image: UIImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        backgroundImage = image
+        (self.view as! UIImageView).contentMode = .center
+        (self.view as! UIImageView).image = backgroundImage
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("cancelled")
+        picker.dismiss(animated: true, completion: nil)
     }
     
     // Places each word from word bank in rows
